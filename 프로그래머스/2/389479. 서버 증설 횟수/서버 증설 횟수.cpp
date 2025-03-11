f0b94ad8ed5ea2bfca02cd8f7bdcc2e252bbebe9
kangrae <kangrae0501@gmail.com>
2025-03-11 18:35:58 +0900

    Update 서버 증설 횟수.cpp

--- 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.cpp
+++ 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.cpp
@@ -4,13 +4,12 @@
 using namespace std;
 
 int solution(vector<int> players, int m, int k) {
-    int answer = 0; // 서버 증설 횟수
-    vector<int> server (25, 1); // 현재 서버
+    int answer = 0;             // 서버 증설 횟수
+    vector<int> server (25, 1); // 현재 서버 개수
     
     for (int time = 0; time < players.size(); time++) {
         int need = players[time] - (server[time] * m);
-        if (need < 0) continue;
-        else {
+        if (need >= 0) {
             int temp = need / m + 1;
             for (int i = 0; i < k && time + i < players.size(); i++) server[time + i] += temp;
             answer += temp;
@@ -19,13 +18,3 @@ int solution(vector<int> players, int m, int k) {
     
     return answer;
 }
-
-/*
-int need = players[time] - (server[time] * m);
-        while (need >= 0) {
-            for (int i=0; i<5 && time+i<players.size(); i++) server[time+i]++;
-            answer++;
-            
-            need = players[time] - (server[time] * m) ;
-        }
-*/
