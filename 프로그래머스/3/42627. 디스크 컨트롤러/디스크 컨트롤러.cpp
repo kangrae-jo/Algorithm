d18784e94d93b912a4a9c90b08f71a289a22d4ad
kangrae <kangrae0501@gmail.com>
2025-03-20 16:44:20 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.13 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.cpp
@@ -0,0 +1,31 @@
+#include <queue>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<vector<int>> jobs) {
+    sort(jobs.begin(), jobs.end());
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
+
+    int sum = 0, time = 0, index = 0;
+    int n = jobs.size();
+
+    while (index < n || !pq.empty()) {
+        while (index < n && jobs[index][0] <= time) {
+            pq.push({jobs[index][1], jobs[index][0]});
+            index++;
+        }
+
+        if (!pq.empty()) {
+            auto [t, s] = pq.top();
+            pq.pop();
+
+            time += t;
+            sum += time - s;        // 대기 시간 + 실행 시간 합산
+        } 
+        else time = jobs[index][0]; // 다음 작업의 요청 시각으로 점프
+    }
+
+    return sum / n;
+}
