6cb02b175c0fcdf425fea41a5fa105a6d04c1778
kangrae <kangrae0501@gmail.com>
2025-05-26 13:52:41 +0900

    [level 2] Title: 서버 증설 횟수, Time: 0.02 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.cpp
+++ 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.cpp
@@ -1,18 +1,18 @@
-#include <string>
 #include <vector>
 
 using namespace std;
 
 int solution(vector<int> players, int m, int k) {
-    int answer = 0;             // 서버 증설 횟수
-    vector<int> server (25, 1); // 현재 서버 개수
+    int answer = 0;
+    vector<int> server(players.size(), 0);
     
     for (int time = 0; time < players.size(); time++) {
-        int need = players[time] - (server[time] * m);
-        if (need >= 0) {
-            int temp = need / m + 1;
-            for (int i = 0; i < k && time + i < players.size(); i++) server[time + i] += temp;
-            answer += temp;
+        int need = (players[time] - server[time] * m) / m;
+        if (need <= 0) continue;
+        
+        answer += need;
+        for (int i = 0; i < k && time + i < players.size(); i++) {
+            server[time + i] += need;
         }
     }
     
