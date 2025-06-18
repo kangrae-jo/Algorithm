b765fb2559f27d17cf459ce21ac719a4bfc3bebb
kangrae <kangrae0501@gmail.com>
2025-06-18 16:19:19 +0900

    [level 3] Title: 풍선 터트리기, Time: 13.89 ms, Memory: 46.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int solution(vector<int> a) {
+    int N = a.size();
+    if (N <= 3) return N; 
+
+    vector<int> leftMin(N), rightMin(N);
+    leftMin[0] = a[0];
+    for (int i = 1; i < N; i++)
+        leftMin[i] = min(leftMin[i-1], a[i]);
+    
+    rightMin[N-1] = a[N-1];
+    for (int i = N-2; i >= 0; i--)
+        rightMin[i] = min(rightMin[i+1], a[i]);
+
+    int answer = 2; // 양 끝 풍선 항상 가능
+    for (int i = 1; i < N-1; i++) {
+        if (a[i] <= leftMin[i-1] || a[i] <= rightMin[i+1]) answer++;
+    }
+    
+    return answer;
+}
