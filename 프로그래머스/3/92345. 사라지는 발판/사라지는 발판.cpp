b70ab6eacbd304cdf126ac6eaeb2bac966be176b
kangrae <kangrae0501@gmail.com>
2025-07-20 15:22:20 +0900

    [level 3] Title: 사라지는 발판, Time: 3.50 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/3/92345. 사라지는 발판/사라지는 발판.cpp
+++ 프로그래머스/3/92345. 사라지는 발판/사라지는 발판.cpp
@@ -13,8 +13,9 @@ bool isIn(int y, int x) { return 0 <= y && y < N && 0 <= x && x < M; }
 
 // {이길 수 있는지, 이동 횟수}
 pair<bool, int> dfs(vector<vector<int>>& board, int aY, int aX, int bY, int bX) {
+    // 내 차례인데 발판이 없으면 패배
     if (board[aY][aX] == EMPTY) {
-        return {false, 0}; // 내 차례인데 발판이 없으면 패배
+        return {false, 0};
     }
     
     bool win = false;
@@ -28,18 +29,21 @@ pair<bool, int> dfs(vector<vector<int>>& board, int aY, int aX, int bY, int bX) 
             auto [oppWin, turns] = dfs(board, bY, bX, aY_, aX_);
             board[aY][aX] = PATH;
 
+            // 상대가 못이기면, 내가 이기는 경우
             if (!oppWin) {      
-                // 상대가 못이기면, 내가 이기는 경우
                 win = true;
                 minTurn = min(minTurn, turns + 1);
-            } else if (!win) {  
-                // 이미 이길 수 있는 방법이 있으면 아래는 안 봐도 됨
+            } 
+            // 이미 이길 수 있는 방법이 있으면 아래는 안 봐도 됨
+            else if (!win) {  
                 maxTurn = max(maxTurn, turns + 1);
             }
         }
     }
+    
     // 이길 수 있으면 최소 턴, 아니면 최대 턴
-    return win ? make_pair(true, minTurn) : make_pair(false, maxTurn);
+    if (win) return {true, minTurn};
+    return {false, maxTurn};
 }
 
 int solution(vector<vector<int>> board, vector<int> aloc, vector<int> bloc) {
