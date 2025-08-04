7f5631369d399ddac0e38b67b736e6b3308015bd
kangrae <kangrae0501@gmail.com>
2025-08-04 14:05:01 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 26.23 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.cpp
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.cpp
@@ -4,29 +4,33 @@
 
 using namespace std;
 
-void move(stack<int>& s, int cap) {
-    int sum = 0, temp = 0;
+void move(stack<int>& s, int capacity) {
+    int carry = 0;
     while (!s.empty()) {
-        temp = s.top();
-        if (sum + temp > cap) {
-            s.pop();
-            s.push(temp - (cap - sum));
+        int cur = s.top();
+        s.pop();
+        
+        // 현재 적재 가능한 만큼만 들고가기
+        if (carry + cur > capacity) {
+            s.push(cur - (capacity - carry));
             break;
         }
-        sum += temp;
-        s.pop();
+        // 다 들고 가기
+        carry += cur;
     }
 }
 
-long long solution(int cap, int n, vector<int> deliveries_, vector<int> pickups_) {  
+long long solution(int cap, int n, vector<int> deliveries_, vector<int> pickups_) { 
+    // 스택으로 표현
     stack<int> deliveries, pickups;
     for (int d : deliveries_) deliveries.push(d);
-    for (int p : pickups_) pickups.push(p);
+    for (int p : pickups_)    pickups.push(p);
     
+    // 배달과 수거
     long long answer = 0;
     while (!deliveries.empty() || !pickups.empty()) {
         while (!deliveries.empty() && deliveries.top() == 0) deliveries.pop();
-        while (!pickups.empty() && pickups.top() == 0) pickups.pop();
+        while (!pickups.empty()    && pickups.top() == 0)    pickups.pop();
         int size = max(deliveries.size(), pickups.size());
         
         move(deliveries, cap);
