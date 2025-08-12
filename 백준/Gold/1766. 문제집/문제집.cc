4ba78b1e568adc876eecd58569e5705443149bda
kangrae <kangrae0501@gmail.com>
2025-08-12 10:59:30 +0900

    [Gold II] Title: 문제집, Time: 76 ms, Memory: 4164 KB -BaekjoonHub

--- 백준/Gold/1766. 문제집/문제집.cc
+++ 백준/Gold/1766. 문제집/문제집.cc
@@ -4,34 +4,42 @@
 
 using namespace std;
 
-int main() {
-    int N, M;
-    cin >> N >> M;
-    vector<vector<int>> graph(N + 1);
-    vector<int> in(N + 1, 0);
-
-    for (int i = 0; i < M; ++i) {
-        int A, B;
-        cin >> A >> B;
-        graph[A].push_back(B);
-        in[B]++;
+vector<int> tSort(vector<vector<int>>& graph, vector<int> indegree, int N) {
+    priority_queue<int, vector<int>, greater<>> pq;
+    for (int i = 1; i <= N; i++) {
+        if (indegree[i] == 0) pq.push(i);
     }
 
-    priority_queue<int, vector<int>, greater<int>> pq;
-    for (int i = 1; i <= N; ++i) {
-        if (in[i] == 0) pq.push(i);
-    }
-
+    vector<int> result;
     while (!pq.empty()) {
         int cur = pq.top();
         pq.pop();
 
-        cout << cur << " ";
+        result.push_back(cur);
 
         for (int next : graph[cur]) {
-            if (--in[next] == 0) pq.push(next);
+            if (--indegree[next] == 0) pq.push(next);
         }
     }
+
+    return result;
+}
+
+int main() {
+    int N, M;
+    cin >> N >> M;
+
+    vector<int> indegree(N + 1, 0);
+    vector<vector<int>> graph(N + 1);
+    for (int i = 0; i < M; i++) {
+        int A, B;
+        cin >> A >> B;
+        graph[A].push_back(B);
+        indegree[B]++;
+    }
+
+    vector<int> answer = tSort(graph, indegree, N);
+    for (int i : answer) cout << i << " ";
 
     return 0;
 }
